f153e7f46a69334e05cffb47503d140613e6bd14
I am root <jaybee.aicpa@gmail.com>
2024-10-21 07:20:45 +0800

    Fix screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        'gradient-radial-custom': 'radial-gradient(circle, #5AC4F3 0%, #023256 100%)',
+      },
     },
   },
   plugins: [],
